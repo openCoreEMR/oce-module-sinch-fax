5d1d2b397035ef99e724920286eca62a85c5a390
Michael A. Smith <michael@opencoreemr.com>
2025-12-19 09:36:05 -0500

    refactor(php): apply rector updates
    A recent version of rector deprecated a rule name. That caused an error.
    Instead of fixing that explicitly, we switch to php rule sets.

--- rector.php
+++ rector.php
@@ -3,30 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Caching\ValueObject\Storage\FileCacheStorage;
-use Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
 use Rector\CodeQuality\Rector\If_\SimplifyIfElseToTernaryRector;
-use Rector\CodingStyle\Rector\FuncCall\ConsistentImplodeRector;
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
-use Rector\Php80\Rector\Catch_\RemoveUnusedVariableInCatchRector;
-use Rector\Php80\Rector\ClassConstFetch\ClassOnThisVariableObjectRector;
-use Rector\Php80\Rector\ClassMethod\AddParamBasedOnParentClassMethodRector;
-use Rector\Php80\Rector\ClassMethod\FinalPrivateToPrivateVisibilityRector;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
-use Rector\Php80\Rector\Class_\StringableForToStringRector;
-use Rector\Php80\Rector\FuncCall\ClassOnObjectRector;
-use Rector\Php80\Rector\Identical\StrEndsWithRector;
-use Rector\Php80\Rector\Identical\StrStartsWithRector;
-use Rector\Php80\Rector\NotIdentical\StrContainsRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
-use Rector\Php81\Rector\Array_\FirstClassCallableRector;
-use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
-use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
-use Rector\Php82\Rector\Class_\ReadOnlyClassRector;
-use Rector\Php82\Rector\Encapsed\VariableInStringInterpolationFixerRector;
-use Rector\Php82\Rector\FuncCall\Utf8DecodeEncodeToMbConvertEncodingRector;
-use Rector\Php82\Rector\New_\FilesystemIteratorSkipDotsRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 use Rector\ValueObject\PhpVersion;
 
 return RectorConfig::configure()
@@ -52,29 +31,9 @@ return RectorConfig::configure()
     )
     ->withPhpVersion(PhpVersion::PHP_82)
     ->withRules([
-        AddParamBasedOnParentClassMethodRector::class,
-        ChangeSwitchToMatchRector::class,
-        ClassOnObjectRector::class,
-        ClassOnThisVariableObjectRector::class,
-        ConsistentImplodeRector::class,
-        FilesystemIteratorSkipDotsRector::class,
-        FinalPrivateToPrivateVisibilityRector::class,
-        FirstClassCallableRector::class,
-        NullToStrictStringFuncCallArgRector::class,
-        OptionalParametersAfterRequiredRector::class,
-        ReadOnlyClassRector::class,
-        ReadOnlyPropertyRector::class,
-        RemoveParentCallWithoutParentRector::class,
-        RemoveUnusedVariableInCatchRector::class,
-        ReturnNeverTypeRector::class,
         SimplifyIfElseToTernaryRector::class,
-        StrContainsRector::class,
-        StrEndsWithRector::class,
-        StrStartsWithRector::class,
-        StringableForToStringRector::class,
-        Utf8DecodeEncodeToMbConvertEncodingRector::class,
-        VariableInStringInterpolationFixerRector::class,
     ])
+    ->withPhpSets()
     ->withSkip([
         __DIR__ . '/vendor',
     ])
